5747c956f61c50c775c8d40d11f0e399a809c8f9
RubaXa <ibnRubaXa@gmail.com>
2015-12-15 14:26:08 +0300

    #18: + tbody & try catch for Ace

--- src/editor.js
+++ src/editor.js
@@ -10,22 +10,26 @@
 		template: feast.parse('<div bem:mod="{attrs.mode}"/>'),
 
 		didMount: function () {
-			var _this = this;
-			var editor = _this.editor = ace.edit(_this.el);
+			try {
+				var _this = this;
+				var editor = _this.editor = ace.edit(_this.el);
 
-			editor.$blockScrolling = Number.POSITIVE_INFINITY;
+				editor.$blockScrolling = Number.POSITIVE_INFINITY;
 
-			editor.setTheme('ace/theme/tomorrow');
-			editor.getSession().setMode('ace/mode/javascript');
-			editor.setOption('maxLines', _this.attrs['max-lines'] || 30);
-			editor.setOption('minLines', _this.attrs['min-lines'] || 4);
+				editor.setTheme('ace/theme/tomorrow');
+				editor.getSession().setMode('ace/mode/javascript');
+				editor.setOption('maxLines', _this.attrs['max-lines'] || 30);
+				editor.setOption('minLines', _this.attrs['min-lines'] || 4);
 
-			editor.on('change', function () {
-				_this.attrs.data.code = editor.getValue();
-			});
+				editor.on('change', function () {
+					_this.attrs.data.code = editor.getValue();
+				});
 
-			editor.setValue(_this.attrs.data.code || '', 1);
-			editor.focus();
+				editor.setValue(_this.attrs.data.code || '', 1);
+				editor.focus();
+			} catch (err) {
+				console.log('[Ace.error]', err);
+			}
 		},
 
 		didUnmount: function () {
